25a07e27be6c3bdb763701fea43a4e60e069b7d5
James Randolph <james.randolph2016@gmail.com>
2018-07-17 23:50:06 +0000

    automatically backed up by learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -43,6 +43,10 @@ class SongsController < ApplicationController
   end
 
   def edit
+    if params[:artist_id]
+      artist = Artist.find_by(id: params[:artist_id])
+      if artist.nil?
+        redirect_to artists_path, alert: "A"
     @song = Song.find(params[:id])
   end
 
